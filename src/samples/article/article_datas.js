e46b0b0ad5e9bdf0fcf66754de4248a952cfafca
Aldi Ramdani <ramdanialdi889@gmail.com>
2025-01-02 04:09:13 +0700

    fix: vercel config

---
+++ src/samples/article/article_datas.js
@@ -0,0 +1,51 @@
+const article_datas = [
+  {
+    article_id: "001",
+    title_article: "Kesehatan mental 1",
+    category: "Mental",
+    sub_category: "Pengobatan",
+    author: "Asep 1",
+    doctor_verificator: {
+      name: "Dr. Aldi",
+      specialization: "Spesialis Gigi",
+    },
+    admin_verificator: {
+      name: "Caca",
+      specialization: "Artikel Verificator"
+    },
+    created: "2024-07-24 13:53:47",
+    updated: "2024-07-24 13:53:47",
+    published: "2024-07-24 13:53:47",
+    viewer: 1,
+    shared: 1,
+    article_status: 1,
+    verification_status: 1,
+    content:
+      "<p><strong>Panduan Mengoptimalkan Website dengan SEO</strong><p>&nbsp;</p></p><p><strong>Apa Itu SEO?</strong><br>Search Engine Optimization (SEO) adalah teknik untuk meningkatkan kualitas dan kuantitas traffic ke sebuah website melalui hasil pencarian organik di mesin pencari seperti Google. Dengan SEO, website Anda memiliki peluang lebih besar untuk muncul di halaman pertama hasil pencarian.</p><p>&nbsp;</p><hr><p>&nbsp;</p><p><strong>Manfaat SEO untuk Website</strong></p><ol><li><strong>Meningkatkan Visibilitas</strong>: Membantu website Anda lebih mudah ditemukan oleh calon pengunjung.</li><li><strong>Traffic Berkualitas</strong>: Mendatangkan pengunjung yang sesuai dengan target pasar Anda.</li><li><strong>Efek Jangka Panjang</strong>: Strategi SEO yang tepat dapat memberikan hasil yang bertahan lama.</li></ol><hr><p><strong>Langkah-Langkah Menerapkan SEO</strong></p><ol><li><strong>Penelitian Kata Kunci</strong><br>Gunakan alat seperti Google Keyword Planner atau Ahrefs untuk menemukan kata kunci yang relevan dengan topik Anda. Pilih kata kunci yang memiliki volume pencarian tinggi namun persaingan rendah.</li><li><strong>Optimasi Konten</strong></li></ol><ul><li>Gunakan kata kunci di judul, paragraf pertama, dan subheading.</li><li>Buat konten yang informatif, menarik, dan relevan.</li><li>Pastikan meta description Anda jelas dan mengundang klik.</li></ul><ol start='3'><li><strong>Perbaikan Teknikal SEO</strong></li></ol><ul><li>Tingkatkan kecepatan loading website.</li><li>Pastikan website responsif untuk perangkat mobile.</li><li>Gunakan struktur URL yang sederhana dan mudah dimengerti.</li></ul><ol start='4'><li><strong>Bangun Backlink Berkualitas</strong><br>Tautan balik dari website terpercaya dapat meningkatkan otoritas domain Anda. Cobalah untuk berkolaborasi dengan situs lain atau membuat konten yang layak untuk dibagikan.</li><figure class='image' data-ckbox-resource-id='IRYfzod3FZf7'><picture><source srcset='https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/140.webp 140w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/280.webp 280w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/420.webp 420w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/560.webp 560w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/700.webp 700w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/840.webp 840w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/980.webp 980w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1120.webp 1120w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1260.webp 1260w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1400.webp 1400w' sizes='(max-width: 1400px) 100vw, 1400px' type='image/webp'><img src='https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1400.png' width='1400' height='1098'></picture></figure><p>&nbsp;</p></ol><hr><p>&nbsp;</p><p><strong>Kesimpulan</strong><br>SEO adalah investasi yang sangat penting untuk meningkatkan performa website Anda. Dengan memahami dan menerapkan langkah-langkah SEO di atas, Anda bisa mendapatkan lebih banyak traffic organik yang berkualitas. Jangan lupa untuk terus memantau perkembangan dan menyesuaikan strategi berdasarkan analitik data.</p>"
+  },
+  {
+    article_id: "002",
+    title_article: "Kesehatan mental 2",
+    category: "Mental",
+    sub_category: "Pengobatan",
+    author: "Asep 2",
+    doctor_verificator: {
+      name: "Dr. Budi",
+    },
+    admin_verificator: {
+      name: "Caca",
+      specialization: "Artikel Verificator"
+    },
+    verification_status: 1,
+    created: "2024-07-24 13:53:47",
+    updated: "2024-07-24 13:53:47",
+    published: "2024-07-24 13:53:47",
+    viewer: 1,
+    shared: 1,
+    article_status: 1,
+    content:
+      "<p><strong>Panduan Mengoptimalkan Website dengan SEO</strong><p>&nbsp;</p></p><p><strong>Apa Itu SEO?</strong><br>Search Engine Optimization (SEO) adalah teknik untuk meningkatkan kualitas dan kuantitas traffic ke sebuah website melalui hasil pencarian organik di mesin pencari seperti Google. Dengan SEO, website Anda memiliki peluang lebih besar untuk muncul di halaman pertama hasil pencarian.</p><p>&nbsp;</p><hr><p>&nbsp;</p><p><strong>Manfaat SEO untuk Website</strong></p><ol><li><strong>Meningkatkan Visibilitas</strong>: Membantu website Anda lebih mudah ditemukan oleh calon pengunjung.</li><li><strong>Traffic Berkualitas</strong>: Mendatangkan pengunjung yang sesuai dengan target pasar Anda.</li><li><strong>Efek Jangka Panjang</strong>: Strategi SEO yang tepat dapat memberikan hasil yang bertahan lama.</li></ol><hr><p><strong>Langkah-Langkah Menerapkan SEO</strong></p><ol><li><strong>Penelitian Kata Kunci</strong><br>Gunakan alat seperti Google Keyword Planner atau Ahrefs untuk menemukan kata kunci yang relevan dengan topik Anda. Pilih kata kunci yang memiliki volume pencarian tinggi namun persaingan rendah.</li><li><strong>Optimasi Konten</strong></li></ol><ul><li>Gunakan kata kunci di judul, paragraf pertama, dan subheading.</li><li>Buat konten yang informatif, menarik, dan relevan.</li><li>Pastikan meta description Anda jelas dan mengundang klik.</li></ul><ol start='3'><li><strong>Perbaikan Teknikal SEO</strong></li></ol><ul><li>Tingkatkan kecepatan loading website.</li><li>Pastikan website responsif untuk perangkat mobile.</li><li>Gunakan struktur URL yang sederhana dan mudah dimengerti.</li></ul><ol start='4'><li><strong>Bangun Backlink Berkualitas</strong><br>Tautan balik dari website terpercaya dapat meningkatkan otoritas domain Anda. Cobalah untuk berkolaborasi dengan situs lain atau membuat konten yang layak untuk dibagikan.</li><figure class='image' data-ckbox-resource-id='IRYfzod3FZf7'><picture><source srcset='https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/140.webp 140w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/280.webp 280w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/420.webp 420w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/560.webp 560w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/700.webp 700w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/840.webp 840w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/980.webp 980w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1120.webp 1120w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1260.webp 1260w,https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1400.webp 1400w' sizes='(max-width: 1400px) 100vw, 1400px' type='image/webp'><img src='https://ckbox.cloud/1756c7ecf283e77166ab/assets/IRYfzod3FZf7/images/1400.png' width='1400' height='1098'></picture></figure><p>&nbsp;</p></ol><hr><p>&nbsp;</p><p><strong>Kesimpulan</strong><br>SEO adalah investasi yang sangat penting untuk meningkatkan performa website Anda. Dengan memahami dan menerapkan langkah-langkah SEO di atas, Anda bisa mendapatkan lebih banyak traffic organik yang berkualitas. Jangan lupa untuk terus memantau perkembangan dan menyesuaikan strategi berdasarkan analitik data.</p>"
+  },
+];
+
+export default article_datas;
